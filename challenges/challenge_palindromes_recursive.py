0322b5923529f2164c073918c2fab4612706ffb8
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2022-07-30 12:08:49 -0300

    req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,19 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    check_odd_string = high_index - low_index
+
+    if(word == ''):
+        return False
+
+    if check_odd_string == 1 and word[low_index] == word[high_index]:
+        return True
+
+    if low_index == high_index:
+        return True
+
+    if low_index == 0 and high_index == 1:
+        return True
+
+    if word[low_index] != word[high_index]: 
+        return False
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
