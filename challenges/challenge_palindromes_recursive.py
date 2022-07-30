d8687351f00c4d50d21ce60075d5849de7926889
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2022-07-30 12:11:49 -0300

    linter

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -10,10 +10,7 @@ def is_palindrome_recursive(word, low_index, high_index):
     if low_index == high_index:
         return True
 
-    if low_index == 0 and high_index == 1:
-        return True
-
-    if word[low_index] != word[high_index]: 
+    if word[low_index] != word[high_index]:
         return False
 
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,10 +1,10 @@
 def study_schedule(permanence_period, target_time):
     count_students = 0
-    if(target_time == None):
+    if(target_time is None):
         return None
     for period in permanence_period:
         if type(period[0]) != int or type(period[1]) != int:
-            return None 
+            return None
         if period[0] <= target_time <= period[1]:
             count_students += 1
     return count_students
