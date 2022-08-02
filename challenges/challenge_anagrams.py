4f3b36e54d7663ec91f000033ed1bb046fa3753e
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2022-08-02 11:35:59 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,15 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_str = list(first_string.lower())
+    second_str = list(second_string.lower())
+
+    
+    if (len(first_str) != len(second_str)):
+        return False
+
+    for value in first_str:
+        try:
+            second_str.remove(value)
+        except ValueError:
+            return False
+
+    return True
